6521f461bdb9dd803bf7cb5b8ddfb72c3a05c492
aniket pathak <aniketpathak1@gmail.com>
2021-08-03 20:13:12 -0700

    Some Python Project

---
+++ SeattlePoliceData.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+filePath = "/Users/aniketpathak/Documents/IMPORTANT/MS/CST-570/Topic-1/Backup.csv"
+
+dataTypes = {
+    "CAD Event Number": "int64",
+    "Event Clearance Description": "string",
+    "Call Type": "string",
+    "Priority": "string",
+    "Initial Call Type": "string",
+    "Final Call Type": "string",
+    "Original Time Queued": "string",
+    "Arrived Time": "string",
+    "Precinct": "string",
+    "Sector": "string",
+    "Beat": "string",
+}
+
+data = pd.read_csv(filepath_or_buffer=filePath, dtype=dataTypes)
+
+sub_set = data[['CAD Event Number', 'Call Type', 'Original Time Queued']].head(1000)
+
+# This will show how many different types of call type are there
+print(sub_set["Call Type"].unique())
+
+# Below will set the different call types to number 1 to 6
+sub_set.loc[(sub_set['Call Type'] == '911'), 'Call Type'] = '1'
+sub_set.loc[(sub_set['Call Type'] == 'TELEPHONE OTHER, NOT 911'), 'Call Type'] = '2'
+sub_set.loc[(sub_set['Call Type'] == 'ONVIEW'), 'Call Type'] = '3'
+sub_set.loc[(sub_set['Call Type'] == 'ALARM CALL (NOT POLICE ALARM'), 'Call Type'] = '4'
+sub_set.loc[(sub_set['Call Type'] == 'TEXT MESSAGE'), 'Call Type'] = '5'
+sub_set.loc[(sub_set['Call Type'] == 'ALARM CALL (NOT POLICE ALARM)'), 'Call Type'] = '6'
+
+print(sub_set["Call Type"].unique())
+
